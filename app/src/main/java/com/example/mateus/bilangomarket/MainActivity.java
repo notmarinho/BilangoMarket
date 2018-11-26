5224bb0ae84b42b8366dd81f46e56e9e83850273
Mateus Marinho <mateus.santos.840@hotmail.com>
2018-11-26 16:48:49 -0200

    Base do aplicativo

---
+++ app/src/main/java/com/example/mateus/bilangomarket/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mateus.bilangomarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
